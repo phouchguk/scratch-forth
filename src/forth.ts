f78e984087a0d6c5a347e8ccb7860454145e8d67
phouchg <phouchg@protonmail.com>
2020-09-05 12:53:20 +0100

    vm class

--- src/forth.ts
+++ src/forth.ts
@@ -1,71 +1,15 @@
-import { CELLL, Mem } from "./mem";
-import { Stack } from "./stack";
+import { Mem } from "./mem";
+import { Vm } from "./vm";
 import { Dict } from "./dict";
 
 const mem = new Mem();
-const stack = new Stack(mem);
 
 const prims = ["BYE", "EXIT", "doLIT", "!", "@", "+", "."];
 const primCount = prims.length;
 
 const dictm = new Dict(primCount, mem);
 
-let running = false;
-
-function step() {
-  const op = mem.get16(mem.PC);
-  mem.PC += CELLL; // point to next instruction
-
-  //const op = dict[pc.dicti][pc.i++];
-
-  // primitive
-  switch (op) {
-    case 0: // BYE
-      running = false;
-      return;
-
-    case 1: // EXIT
-      mem.PC = stack.popr();
-      return;
-
-    case 2: // doLIT
-      stack.pushd(mem.get16(mem.PC));
-      mem.PC += CELLL;
-      return;
-
-    case 3: // !
-      const addr = stack.popd();
-      mem.set16(addr, stack.popd());
-      return;
-
-    case 4: // @
-      stack.pushd(mem.get16(stack.popd()));
-      return;
-
-    case 5: // +
-      stack.pushd(stack.popd() + stack.popd());
-      return;
-
-    case 6: // .
-      console.log(stack.popd());
-      return;
-
-    default:
-      // run compound proc
-      stack.pushr(mem.PC);
-      mem.PC = op;
-  }
-}
-
-function run() {
-  running = true;
-
-  while (running) {
-    step();
-  }
-}
-
-function compile(x) {
+function compile(x: string) {
   const n = parseInt(x, 10);
 
   if (isNaN(n)) {
@@ -76,27 +20,26 @@ function compile(x) {
   return n;
 }
 
-const trim = (s) => s.trim();
+const trim = (s: string) => s.trim();
 
-function parse(s) {
+function parse(s: string) {
   return s.split(" ").map(trim).map(compile);
 }
 
 // compounds
 const add5 = "doLIT 5 + EXIT";
 const add5c = parse(add5);
-console.log("ADD5", add5c);
 dictm.add("ADD5", add5c);
 
 const testn = "TEST";
-const test = "doLIT 1 ADD5 doLIT 6 ! doLIT 6 @ doLIT 2 + doLIT 6 @ + . BYE";
+const test = "doLIT 1 ADD5 doLIT 6 ! doLIT 6 @ doLIT 3 + doLIT 6 @ + . BYE";
 const testc = parse(test);
-console.log("TEST", testc);
 dictm.add(testn, testc);
 
 // run the test code - lookup returns code address
 mem.PC = dictm.lookup(testn);
 
-run();
+const vm = new Vm(mem);
+vm.run();
 
 // facility to forget last dict item (for repl)
---
+++ src/vm.ts
@@ -0,0 +1,64 @@
+import { CELLL, Mem } from "./mem";
+import { Stack } from "./stack";
+
+export class Vm {
+  private mem: Mem;
+  private stack: Stack;
+  private running: boolean = false;
+
+  constructor(mem: Mem) {
+    this.mem = mem;
+    this.stack = new Stack(this.mem);
+  }
+
+  step() {
+    const op = this.mem.get16(this.mem.PC);
+    this.mem.PC += CELLL; // point to next instruction
+
+    // primitive
+    switch (op) {
+      case 0: // BYE
+        this.running = false;
+        return;
+
+      case 1: // EXIT
+        this.mem.PC = this.stack.popr();
+        return;
+
+      case 2: // doLIT
+        this.stack.pushd(this.mem.get16(this.mem.PC));
+        this.mem.PC += CELLL;
+        return;
+
+      case 3: // !
+        const addr = this.stack.popd();
+        this.mem.set16(addr, this.stack.popd());
+        return;
+
+      case 4: // @
+        this.stack.pushd(this.mem.get16(this.stack.popd()));
+        return;
+
+      case 5: // +
+        this.stack.pushd(this.stack.popd() + this.stack.popd());
+        return;
+
+      case 6: // .
+        console.log(this.stack.popd());
+        return;
+
+      default:
+        // run compound proc
+        this.stack.pushr(this.mem.PC);
+        this.mem.PC = op;
+    }
+  }
+
+  run() {
+    this.running = true;
+
+    while (this.running) {
+      this.step();
+    }
+  }
+}
