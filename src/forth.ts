b1f096bf7939325ad5ee7f02abeb153d1794988c
phouchg <phouchg@protonmail.com>
2020-09-05 12:34:46 +0100

    use mem code

--- src/forth.ts
+++ src/forth.ts
@@ -1,84 +1,66 @@
-import { Mem } from "./mem";
+import { CELLL, Mem } from "./mem";
 import { Stack } from "./stack";
 import { Dict } from "./dict";
 
 const mem = new Mem();
 const stack = new Stack(mem);
 
-const prims = ["EXIT", "doLIT", "!", "@", "+", "."];
+const prims = ["BYE", "EXIT", "doLIT", "!", "@", "+", "."];
 const primCount = prims.length;
 
 const dictm = new Dict(primCount, mem);
 
-const dict = [];
-
-const rs = [];
-
-let pc = null;
-
-// add prims
-for (let i = 0; i < primCount; i++) {
-  dict.push([prims[i], i]);
-}
-
-function lookup(name) {
-  for (let i = dict.length - 1; i >= 0; i--) {
-    if (dict[i][0] === name) {
-      return i;
-    }
-  }
-
-  throw new Error(`${name} ?`);
-}
-
-function ptr(dicti) {
-  return { dicti, i: 1 }; // 1 cus skip name
-}
+let running = false;
 
 function step() {
-  const op = dict[pc.dicti][pc.i++];
+  const op = mem.get16(mem.PC);
+  mem.PC += CELLL; // point to next instruction
+
+  //const op = dict[pc.dicti][pc.i++];
 
   // primitive
   switch (op) {
-    case 0: // EXIT
-      if (rs.length === 0) {
-        pc = null;
-      } else {
-        pc = rs.pop();
-      }
-
+    case 0: // BYE
+      running = false;
       return;
 
-    case 1: // doLIT
-      stack.pushd(dict[pc.dicti][pc.i++]);
+    case 1: // EXIT
+      mem.PC = stack.popr();
       return;
 
-    case 2: // !
+    case 2: // doLIT
+      stack.pushd(mem.get16(mem.PC));
+      mem.PC += CELLL;
+      return;
+
+    case 3: // !
       const addr = stack.popd();
       mem.set16(addr, stack.popd());
       return;
 
-    case 3: // @
+    case 4: // @
       stack.pushd(mem.get16(stack.popd()));
       return;
 
-    case 4: // +
+    case 5: // +
       stack.pushd(stack.popd() + stack.popd());
       return;
 
-    case 5: // .
+    case 6: // .
       console.log(stack.popd());
       return;
 
     default:
       // run compound proc
-      rs.push(pc);
-      pc = ptr(op);
+      stack.pushr(mem.PC);
+      mem.PC = op;
   }
 }
 
 function run() {
-  while (pc !== null) {
+  running = true;
+
+  while (running) {
     step();
   }
 }
@@ -88,7 +70,7 @@ function compile(x) {
 
   if (isNaN(n)) {
     const pi = prims.indexOf(x);
-    return pi > -1 ? pi : lookup(x);
+    return pi > -1 ? pi : dictm.lookup(x);
   }
 
   return n;
@@ -97,30 +79,24 @@ function compile(x) {
 const trim = (s) => s.trim();
 
 function parse(s) {
-  return (s + " EXIT").split(" ").map(trim).map(compile);
+  return s.split(" ").map(trim).map(compile);
 }
 
 // compounds
-const add5n = "ADD5";
-const add5 = "doLIT 5 +";
+const add5 = "doLIT 5 + EXIT";
 const add5c = parse(add5);
-
-dictm.add(add5n, add5c);
-add5c.unshift(add5n);
-dict.push(add5c);
-
-pc = ptr(dict.length);
+console.log("ADD5", add5c);
+dictm.add("ADD5", add5c);
 
 const testn = "TEST";
-const test = "doLIT 1 ADD5 doLIT 6 ! doLIT 6 @ doLIT 2 + doLIT 6 @ + .";
+const test = "doLIT 1 ADD5 doLIT 6 ! doLIT 6 @ doLIT 2 + doLIT 6 @ + . BYE";
 const testc = parse(test);
+console.log("TEST", testc);
 dictm.add(testn, testc);
-testc.unshift(testn);
-dict.push(testc);
+
+// run the test code - lookup returns code address
+mem.PC = dictm.lookup(testn);
 
 run();
 
-dict.pop();
-
-console.log(dictm.lookup(testn));
-console.log(dictm.lookup(add5n));
+// facility to forget last dict item (for repl)
