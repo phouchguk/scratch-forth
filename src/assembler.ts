021e47743af046bb9f87eb3bbfbdc3bb7e890073
phouchg <phouchg@protonmail.com>
2020-09-19 16:45:07 +0100

    ldi test

--- src/assembler.ts
+++ src/assembler.ts
@@ -10,11 +10,19 @@ export class Assembler {
     this.mem = mem;
   }
 
-  lda(b: number, a: number) {
-    this.mem.set8(this.ap++, Op.LDA);
+  private ba(b: number, a: number, op: Op) {
+    this.mem.set8(this.ap++, op);
     this.mem.set8(this.ap++, b);
 
     this.mem.set16(this.ap, a);
     this.ap += CELLL;
+  }
+
+  lda(b: number, a: number) {
+    this.ba(b, a, Op.LDA);
+  }
+
+  ldi(b: number, a: number) {
+    this.ba(b, a, Op.LDI);
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -4,12 +4,17 @@ import { EM, Mem } from "./mem";
 import { Assembler } from "./assembler";
 
 const mem = new Mem(new ArrayBuffer(EM));
-
 const asm = new Assembler(mem);
+
+const io = new Console();
+const vm = new Vm(mem, io);
+
+// TEST LDA
 
 mem.SP = 100;
 mem.WP = 42;
 
+console.log("LDA");
 console.log("SP", mem.SP);
 console.log("WP", mem.WP);
 
@@ -17,9 +22,31 @@ mem.PC = asm.ap;
 
 asm.lda(Reg.SP, Reg.WP);
 
-const io = new Console();
-const vm = new Vm(mem, io);
 vm.run();
 
+console.log("result:");
 console.log("SP", mem.SP);
 console.log("WP", mem.WP);
+
+// TEST LDI
+
+mem.SP = 100;
+mem.WP = 42;
+mem.set16(42, 99);
+
+console.log();
+console.log("LDI");
+console.log("SP", mem.SP);
+console.log("WP", mem.WP);
+console.log("42", mem.get16(42));
+
+mem.PC = asm.ap;
+
+asm.ldi(Reg.SP, Reg.WP);
+
+vm.run();
+
+console.log("result:");
+console.log("SP", mem.SP);
+console.log("WP", mem.WP);
+console.log("42", mem.get16(42));
